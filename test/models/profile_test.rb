2aea58b54d1e8f11a963b1f6445b30387b093d63
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-04-16 16:08:45 -0300

    R/G/Rf/C TDD build catchup after auto-installers done.

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -4,6 +4,9 @@ class ProfileTest < ActiveSupport::TestCase
 
   test "a profile responds to userID" do
     profile = Profile.new
-    assert profile.responds user_id
+    assert profile.respond_to? :user_id
+    assert profile.respond_to? :discord_id
+    assert profile.respond_to? :steam_id
+    assert profile.respond_to? :du_charname
   end
 end
