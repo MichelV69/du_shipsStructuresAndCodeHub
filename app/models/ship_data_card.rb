054153154b508fc9d26515820bfae33d2789493c
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-04-17 14:31:35 -0300

    files for ship_data_cards that didn't get picked up in the last commit for some reason.

---
+++ app/models/ship_data_card.rb
@@ -0,0 +1,3 @@
+class ShipDataCard < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20240417024234_create_ship_data_cards.rb
@@ -0,0 +1,16 @@
+class CreateShipDataCards < ActiveRecord::Migration[7.1]
+  def change
+    create_table :ship_data_cards do |t|
+      t.integer :user_id
+      t.string :design_name
+      t.integer :price_blueprint
+      t.integer :price_token
+      t.integer :core_size
+      t.boolean :env_atmosphere
+      t.boolean :env_ground
+      t.boolean :space
+
+      t.timestamps
+    end
+  end
+end
