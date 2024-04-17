a1e1b987421990ee1f38820cbbeb4296cacf415f
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-04-17 19:54:50 -0300

    Green : "the user can set multiple roles for an SDC"

--- app/models/vehicle_role.rb
+++ app/models/vehicle_role.rb
@@ -2,7 +2,7 @@ class VehicleRole < ApplicationRecord
   belongs_to :ship_data_card
 
   def set_role_by_sym symbol
-    roles_list = {combat: 1, Hauler: 2, Courier: 3, Carrier: 4, Racer: 5, Pocket: 6}
+    roles_list = {pvp_combat: 1, pve_combat: 2, hauler: 3, courier: 4, carrier: 5, racer: 6, pocket: 7, prospector: 8, tile_scanner: 9 }
     self.role_name = roles_list[symbol]
   end
 end
--- test/models/vehicle_role_test.rb
+++ test/models/vehicle_role_test.rb
@@ -22,4 +22,25 @@ class VehicleRoleTest < ActiveSupport::TestCase
     vr_list = @u.ship_data_cards.first.vehicle_roles.all
     assert_equal 1, vr_list.count, "Wrong list count"
   end
+
+  test "the user can set multiple roles for an SDC" do
+    vr = VehicleRole.new
+    vr.ship_data_card_id = @sdc.id
+    vr.set_role_by_sym :pvp_combat
+    assert vr.save!, "Failed to save"
+
+    vr2 = VehicleRole.new
+    vr2.ship_data_card_id = @sdc.id
+    vr2.set_role_by_sym :prospector
+    assert vr2.save!, "Failed to save"
+
+    vr3 = VehicleRole.new
+    vr3.ship_data_card_id = @sdc.id
+    vr3.set_role_by_sym :hauler
+    assert vr3.save!, "Failed to save"
+
+    vr_list = @u.ship_data_cards.first.vehicle_roles.all
+    assert_equal 3, vr_list.count, "Wrong list count"
+  end
+
 end
