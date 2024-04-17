d772cee95040f87415907416ac89e07655442b82
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-04-17 20:35:30 -0300

    Green: "the model recognizes a list of different roles"

--- test/models/vehicle_role_test.rb
+++ test/models/vehicle_role_test.rb
@@ -43,4 +43,15 @@ class VehicleRoleTest < ActiveSupport::TestCase
     assert_equal 3, vr_list.count, "Wrong list count"
   end
 
+  test "the model recognizes a list of different roles" do
+    role_test_list = {pvp_combat: 1, pve_combat: 2, hauler: 3, courier: 4, carrier: 5,
+    racer: 6, pocket: 7, prospector: 8, tile_scanner: 9 }
+
+    vr = VehicleRole.new
+    vr.ship_data_card_id = @sdc.id
+
+    role_test_list.keys.each do |k|
+      assert vr.set_role_by_sym(k), "Unable to set ${k}"
+    end
+  end
 end
