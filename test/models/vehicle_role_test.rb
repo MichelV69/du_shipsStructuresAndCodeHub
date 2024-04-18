4f8a253455bbf86016c1b0de9083af614c9fd8c8
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-04-17 21:14:37 -0300

    fix now-failing test

--- test/models/vehicle_role_test.rb
+++ test/models/vehicle_role_test.rb
@@ -16,7 +16,7 @@ class VehicleRoleTest < ActiveSupport::TestCase
   test "the user can set a role for an SDC" do
     vr = VehicleRole.new
     vr.ship_data_card_id = @sdc.id
-    vr.set_role_by_sym :combat
+    vr.set_role_by_sym :pvp_combat
     assert vr.save!, "Failed to save"
 
     vr_list = @u.ship_data_cards.first.vehicle_roles.all
