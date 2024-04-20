6abce4d4adbe9f6a773ebfb9f684ab08a07f1d79
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2024-04-20 00:27:52 -0300

    Green: "visiting the index while not logged in, I should see a login link"

--- test/system/landing_pages_test.rb
+++ test/system/landing_pages_test.rb
@@ -1,10 +1,19 @@
 require "application_system_test_case"
 
 class LandingPagesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the index should see app and landing page titles" do
     visit "/"
 
     assert_selector "h1", text: "Builder's Bay"
     assert_selector "h2", text: "LandingPage"
+  end
+
+  test "visiting the index while not logged in, I should see a login link" do
+    visit "sign_out"
+    sleep 0.5
+    visit "/"
+
+    assert_selector "div.user_card h3", text: "Not signed in"
+    assert_selector "div.login_prompt", text: "Sign in, please"
   end
 end
